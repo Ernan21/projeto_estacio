3e171b920386a7935b2a4e5a068990d85a3fa05e
Marcelo Sampaio <marcelosampaiocontato@gmail.com>
2024-06-16 15:58:22 -0300

    Reajuste nos parâmetros da tela cadastro

--- src/main/resources/static/javascript/produto-cadastrado.js
+++ src/main/resources/static/javascript/produto-cadastrado.js
@@ -1,56 +1,68 @@
 function submitForm() {
     var descricao = document.getElementById("descricao_completa").value;
-    var estoque = parseFloat(document.getElementById("estoque").value);
-    var custo_produto = parseFloat(document.getElementById("custo_produto").value);
-    var preco_venda = parseFloat(document.getElementById("preco_venda").value);
+    var estoque = document.getElementById("estoque").value;
+    var custoProduto = document.getElementById("custo_produto").value;
+    var precoVenda = document.getElementById("preco_venda").value;
     var messageArea = document.getElementById("messageArea");
 
-    if (!(estoque > 0)) {
-        showMessage("Por favor, insira uma quantidade maior que zero.", "error");
-        return false;
-    }
-    if (!(custo_produto > 0)) {
-        showMessage("Por favor, insira um custo do produto maior que zero.", "error");
-        return false;
-    }
-    if (!(preco_venda > 0)) {
-        showMessage("Por favor, insira um preço de venda maior que zero.", "error");
-        return false;
-    }
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                if (response.exists) {
-                    showMessage("Produto já cadastrado!", "error");
-                } else {
-                    showMessage("Produto cadastrado com sucesso!", "success");
-                    setTimeout(function() {
-                        window.location.href = "/screen_cadastro";
-                    }, 3000);
-                }
-            } else {
-                console.error('Erro ao realizar a solicitação.');
-            }
+    fetch("/check-description", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ descricao: descricao }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao verificar a descrição do produto. Status: ' + response.status);
         }
-    };
-    xhr.open("POST", "/check-description", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({ descricao: descricao, estoque: estoque, custo_produto: custo_produto, preco_venda: preco_venda }));
+        return response.json();
+    })
+    .then(data => {
+        if (data.exists) {
+            messageArea.innerText = "Produto já cadastrado!";
+            messageArea.classList.remove("success");
+            messageArea.classList.add("error");
+        } else {
+            if (estoque <= 0) {
+                messageArea.innerText = "Quantidade de Estoque deve ser maior que zero.";
+                messageArea.classList.remove("success");
+                messageArea.classList.add("error");
+                return;
+            }
+
+            if (custoProduto <= 0) {
+                messageArea.innerText = "Custo do Produto deve ser maior que zero.";
+                messageArea.classList.remove("success");
+                messageArea.classList.add("error");
+                return; 
+            }
+
+            if (precoVenda <= 0) {
+                messageArea.innerText = "Preço de Venda deve ser maior que zero.";
+                messageArea.classList.remove("success");
+                messageArea.classList.add("error");
+                return; 
+            }
+
+            messageArea.innerText = "Produto cadastrado com sucesso!";
+            messageArea.classList.remove("error");
+            messageArea.classList.add("success");
+            messageArea.style.display = "block";
+
+            setTimeout(() => {
+                document.getElementById("cadastroForm").submit();
+            }, 1500);
+        }
+        messageArea.style.display = "block";
+    })
+    .catch(error => {
+        console.error('Erro ao realizar a solicitação:', error);
+        messageArea.innerText = "Ocorreu um erro ao verificar a descrição do produto. Tente novamente mais tarde.";
+        messageArea.classList.remove("success");
+        messageArea.classList.add("error");
+        messageArea.style.display = "block";
+    });
 
     return false;
-}
-
-function showMessage(message, type) {
-    var messageArea = document.getElementById("messageArea");
-    messageArea.innerText = message;
-    messageArea.classList.remove("success", "error");
-    messageArea.classList.add(type);
-    messageArea.style.display = "block";
-
-    setTimeout(function() {
-        messageArea.style.display = "none";
-    }, 3000);
 }
